1793ec461a0d3767c7bf809f20514f342585b29f
Rebecca Turner <me@re-becca.org>
2016-03-04 17:07:58 -0800

    test: rewrite outdated-depth-deep for windows
    Stop using the not-safe-for-windows `npm explore`.
    Stop using the possibly unstable internal APIs and use
    `common-npm` instead.

--- test/tap/outdated-depth-deep.js
+++ test/tap/outdated-depth-deep.js
@@ -2,7 +2,6 @@ var common = require('../common-tap')
 var path = require('path')
 var test = require('tap').test
 var rimraf = require('rimraf')
-var npm = require('../../')
 var mr = require('npm-registry-mock')
 var pkg = path.resolve(__dirname, 'outdated-depth-deep')
 var cache = path.resolve(pkg, 'cache')
@@ -41,38 +40,54 @@ test('setup', function (t) {
 })
 
 test('outdated depth deep (9999)', function (t) {
-  var underscoreOutdated = ['underscore', '1.3.1', '1.3.1', '1.5.1', '1.3.1']
+                          // wanted,            has,                latest
+  var underscoreOutdated = ['underscore@1.3.1', 'underscore@1.3.1', 'underscore@1.5.1']
   var childPkg = path.resolve(pkg, 'node_modules', 'npm-test-peer-deps')
 
-  var expected = [ [childPkg].concat(underscoreOutdated).concat([null]),
-                   [pkg].concat(underscoreOutdated).concat([null]) ]
+  var expected = [ [path.join(childPkg, 'node_modules', 'underscore')].concat(underscoreOutdated),
+                   [path.join(pkg, 'node_modules', 'underscore')].concat(underscoreOutdated) ]
 
-  process.chdir(pkg)
-
-  mr({ port: common.port }, function (er, s) {
-    npm.load({
-      cache: cache,
-      loglevel: 'silent',
-      registry: common.registry,
-      depth: 9999
-    },
-    function () {
-      npm.install('.', function (er) {
-        if (er) throw new Error(er)
-        var nodepath = process.env.npm_node_execpath || process.env.NODE || process.execPath
-        var clibin = path.resolve(__dirname, '../../bin/npm-cli.js')
-        npm.explore('npm-test-peer-deps', nodepath, clibin, 'install', 'underscore', function (er) {
-          if (er) throw new Error(er)
-          npm.outdated(function (err, d) {
-            if (err) throw new Error(err)
-            t.deepEqual(d, expected)
-            s.close()
-            t.end()
-          })
-        })
-      })
-    })
+  var server
+  mr({ port: common.port }, function (err, s) {
+    if (err) throw err
+    server = s
+    thenInstallTestDir()
   })
+
+  var npmArgs = ['--cache=' + cache, '--loglevel=silent', '--registry=' + common.registry, '--depth=9999']
+
+  function thenInstallTestDir (s) {
+    var opts = {stdio: [0, 1, 2], cwd: pkg}
+    common.npm(npmArgs.concat(['install', '.']), opts, function (err, code) {
+      if (err) throw err
+      t.is(code, 0, 'install ok')
+      thenInstallUnderscore()
+    })
+  }
+
+  function thenInstallUnderscore () {
+    var opts = {
+      cwd: path.join(pkg, 'node_modules', 'npm-test-peer-deps'),
+      stdio: [0, 1, 2]
+    }
+    common.npm(npmArgs.concat(['install', 'underscore']), opts, function (err, code) {
+      if (err) throw err
+      t.is(code, 0, 'explore & install ok')
+      thenOutdated()
+    })
+  }
+
+  function thenOutdated () {
+    var opts = { stdio: [0, 'pipe', 2], cwd: pkg }
+    common.npm(npmArgs.concat(['--parseable', 'outdated']), opts, function (err, code, stdout, stderr) {
+      if (err) throw err
+      t.is(code, 0, 'outdated ran ok')
+      var result = stdout.trim().split(/\n/).map(function (line) { return line.split(/:/) })
+      t.deepEqual(result, expected)
+      server.close()
+      t.end()
+    })
+  }
 })
 
 test('cleanup', function (t) {
